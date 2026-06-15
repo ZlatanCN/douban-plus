bb9f9384544b9ca395038ffe46b9c9397434afb1
Gabriel Zhu <gabriel.jy.zhu@icloud.com>
2026-06-15 18:02:00 +0800

    refactor(project): 重命名并调整模块配置文件路径
    - 将项目模块文件从 douban-appletv-beautify.iml 重命名为 douban-plus.iml
    - 更新模块配置中的 content 路径以匹配新模块名称
    - 调整排除文件夹路径以适应新模块目录结构
    - 修改 package.json 中的 test 脚本，加入 ultracite 检查命令
    - 修正 package.json 中的 author 字段信息
    - 修正用户脚本描述注释中的拼写错误

--- douban-plus.user.js
+++ douban-plus.user.js
@@ -2,7 +2,7 @@
 // @name         Douban Plus
 // @namespace    https://github.com/zhujiayou/douban-appletv-beautify
 // @version      1.0.1
-// @description  将豆瓣电影/电视剧详情页重塑为 Apple TV+ 风格的高级沉浸式页面，保留豆瓣绿作为强调色
+// @descriptionji  将豆瓣电影/电视剧详情页重塑为 Apple TV+ 风格的高级沉浸式页面，保留豆瓣绿作为强调色
 // @author       Sisyphus
 // @match        *://movie.douban.com/subject/*
 // @run-at       document-end
